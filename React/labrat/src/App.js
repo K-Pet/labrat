24d6b8e44e1dcf385bffc7ac962151275387b9c2
Kobe Petrus <kobe.petrus07@gmail.com>
2023-11-18 21:53:41 -0500

    boilerplate react + user auth for api

---
+++ React/labrat/src/App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import api from './api';
+
+
+const App = () => {
+  const [data, setData] = useState([]);
+  cons
+}
+
+
+export default App;
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import uuid
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from typing import Annotated, Any, Dict, List
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, ConfigDict
+import auth
 from database import SessionLocal, engine
 import models
 from fastapi.middleware.cors import CORSMiddleware
+from auth import get_current_user
 
 app = FastAPI()
+app.include_router(auth.router)
+
 
 origins = [
     'http://localhost:3000',
@@ -80,8 +84,16 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 models.Base.metadata.create_all(bind=engine)
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not logged in")
+    return {"User": user}
+
 
 @app.post("/datacenters/", response_model=Datacenter)
 def create_datacenter(datacenter: Datacenter, lab_ids: List[str], db: db_dependency):
